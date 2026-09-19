23593e574c8dd36c3c37d1fb6ad1dd42e52b6be5
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-09-19 17:32:24 +0300

    Message-routing-added

--- backEnd/src/routes/message.route.js
+++ backEnd/src/routes/message.route.js
@@ -1,9 +1,14 @@
 import express from "express"
+import { getAllContacts, getChatsByuserId, sendMessage, getChatpartners } from "../controller/message.controllers.js"
+import { protectRoute } from "../middleware/auth.middleware.js"
+import { arcjetProjection } from "../middleware/arcjet.middleware.js"
+const router = express.Router()
+router.use(arcjetProjection , protectRoute)
+router.get("/contact", getAllContacts)
+router.get("/chat", getChatpartners)
+router.get("/:id", getChatsByuserId)
 
-const router  = express.Router()
+router.post("/send/:id", sendMessage)
 
-router.get("/send" ,(req,res) => {
-    return res.json("Message is sending into")
-})
 
 export default router
