d756d551b120b2919a2d46eebd6b85939ba9dfd2
Shucayb-ayuub123 <shucayb594@gmail.com>
2026-09-24 10:54:40 +0300

    FrontEnd signup section

--- frontEnd/src/pages/ChatPage.jsx
+++ frontEnd/src/pages/ChatPage.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const ChatPage = () => {
   return (
     <div>
+
+      Chat Home
       
     </div>
   )
--- frontEnd/src/store/useAuthstore.js
+++ frontEnd/src/store/useAuthstore.js
@@ -1,10 +1,37 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/Axios.js"
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
-    authUser : {name : "shu'aib" , age: 30},
-    isLoading : false,
-    Login : () => {
-        console.log("is Login to every body")
-        set({isLoading : true})
+    authUser: null,
+    isCheckingAuth: true,
+    isSigingnUp: false,
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({ authUser: res.data });
+        } catch (error) {
+            console.log("Error in auth check:", error);
+            set({ authUser: null });
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
+    signup: async (data) => {
+        set({ isSigingnUp: true })
+        try {
+            const res = await axiosInstance.post("/auth/singUp", data)
+            set({ authUser: res.data })
+            toast.success("Account created")
+        } catch (error) {
+
+            toast.error(error.response.data.message)
+
+        } finally {
+            set({ isSigingnUp: false })
+        }
     }
-}))
+
+
+}));
